94f6ea039f0facc4a3b79620e9ec41a4b835dd4d
umanalash <nalash140302@gmail.com>
2023-06-21 18:51:31 +0530

    online quiz

---
+++ onlinequiz/onlinequiz.js
@@ -0,0 +1,184 @@
+const submitbutton = document.getElementsByClassName("submit");
+const clearbutton = document.querySelector("clear");
+
+
+class topic1  {
+    constructor(_content1)
+    {
+        this.Question;
+        this.Answer1;
+        this.Answer2;
+        this.Answer3;
+    }
+}
+class topic2  {
+    constructor(_content2)
+    {
+        this.Question;
+        this.Answer1;
+        this.Answer2;
+        this.Answer3;   
+    }
+}
+class topic3  {
+    constructor(_content3)
+    {
+        this.Question;
+        this.Answer1;
+        this.Answer2;
+        this.Answer3;
+    }
+}
+
+
+new topic1(
+Question1=" What is the latest version of HTML?",
+Answer1="!DOCTYPE html",
+Answer2="!DOCTYPE html5",
+Answer3="!DOCTYPE html6"),
+
+new topic1 ( 
+Question2="What is the use of br tag?",
+Answer1="To break the line",
+Answer2="To break the statement",
+Answer3="To break the another tag")
+
+new topic1(
+Question3="How many types in list?",
+Answer1="2",
+Answer2="3",
+Answer3="4")
+
+new topic1(
+Question4="How to write a header in HTML?",
+Answer1="header",
+Answer2="head",
+Answer3="hr")
+
+new topic1(
+Question5="How to mention the title tag?",
+Answer1="title",
+Answer2="tl",
+Answer3="td")
+
+new topic1(
+Question6="Click the odd one out:",
+Answer1="header",
+Answer2="title",
+Answer3="border")
+
+new topic1( 
+Question7="How to use the videos in HTML?",
+Answer1="video",
+Answer2="audio",
+Answer3="media")
+
+new topic1( 
+Question8="How to use the table tag?",
+Answer1="table",
+Answer2="tr",
+Answer3="td")
+
+new topic2(
+Question1="Choose the Vijay movie:",
+Answer1="Beast",
+Answer2="Anjaan",
+Answer3="Aaru")
+
+new topic2( 
+Question2="Who was the first producer of Arya-Mathavan starrer \"VETTAI\"?",
+Answer1="Udhayanithi Stalin",
+Answer2="Dhayanidhi Alagiri",
+Answer3="Director Shankar")
+
+new topic2( 
+Question3="What are the vijay movies with trisha?",
+Answer1="Kovil,Kaval",
+Answer2="Kuruvi,Ghilli",
+Answer3="Velu,Valu")
+
+new topic2(
+Question4="Who are the three heroes in the Tamil remake of DELHI BELLY?",
+Answer1="Arya-Sandhanam-Premgi",
+Answer2="Arya-Jiiva-Premgi",
+Answer3="Arya-Santhanam-Nassar")
+
+new topic2(
+Question5="Which was the first Indian and Tamil film,that had bought make-up men from USA?",
+Answer1="Jeans",
+Answer2="Indian",
+Answer3="Gentleman")
+
+new topic2(
+Question6="Which Tamil blockbuster was dubbed and released in Telugu as\"RANGAM\"?",
+Answer1="Mankatha",
+Answer2="Ko",
+Answer3="Ayan")
+
+new topic2(
+Question7="Which is the only actor in Indian cinema to give 5 mega-hits in the year 1982 including Tamil and Hindi?",
+Answer1="Ranjinikanth",
+Answer2="Kamal Haasan",
+Answer3="Mohan Lal"
+)
+
+new topic2(
+Question8="For which movie did actor Surya get more number of awards from various association?",
+Answer1="Perazhagan",
+Answer2="Vaaranam Aayiram",
+Answer3="Ghajini",
+)
+
+new topic3( 
+Question1="Who is the youngest music composer joining the film industry at the age of 16?",
+Answer1="Harris Jeyaraj",
+Answer2="Yuvan Shankar Raja",
+Answer3="G.V.Prakash")
+
+new topic3(
+Question2="En aayul ragai neeayadi enaani veradi summai thangum enthan kanmani",
+Answer1="Mayakam Enna",
+Answer2="Moonu(3)",
+Answer3="Nanban")
+
+new topic3(
+Question3="Thennattu ayyangathum chettinatu samayal vasam newyorkai thandi kooda mookai thoolaikum",
+Answer1="Sillunu Oru Kadhal",
+Answer2="Amarkalam",
+Answer3="Anantha Thandavam")
+
+new topic3(      
+Question4="Aagaya suriyanai otrai jadaiyil kattiyaval nindralum vinmeenai netri suttiyil otiyaval",
+Answer1="Saamurai",
+Answer2="Rythm",
+Answer3="Mazhai")
+
+new topic3(       
+Question5="Nadhiyae nadhiyae kadhal nadhiyae neeyum pen thanae adi neeyum pen thanae",
+Answer1="Rythm",
+Answer2="Mudhalvan",
+Answer3="Maanagaram")
+
+new topic3(        
+Question6="Ulagamellam alagaga unnalae therigirathey thudkira idhayathin oosaigal neeyae",
+Answer1="Thalainagaram",
+Answer2="Snehithanae",
+Answer3="Vallavan")
+
+new topic3(
+Question7="En ulagam thanimai kadu nee vandhai pookalodu ini thodarum kanavugalodu pennae pennae",
+Answer1="Angadi Theru",
+Answer2="Ariyasanam",
+Answer3="Aathoram")
+
+new topic3(
+Question8="Thirudanae unnai arinthaen thirudinae ennai arinthaen inunee thirudathanae asai arinthaen",
+Answer1="Thiruppachi",
+Answer2="Aathi",
+Answer3="Kuruvi")
+
+console.log(topic1)
+
+
+
+
