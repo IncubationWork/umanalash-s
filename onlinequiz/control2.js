2ba67fd57b2d45c1fb5b0bddba679b9ac7272483
umanalash <nalash140302@gmail.com>
2023-07-11 16:15:00 +0530

    change

--- onlinequiz/control2.js
+++ onlinequiz/control2.js
@@ -1,45 +1,45 @@
 function  loadQuestion() {
-    const currentQuestionData = getCurrentQuestion();
-    renderQuestion(currentQuestionData);
-  }
-  
-  function checkAnswer(event) {
-    const selectedOption = event.target;
-    const selectedValue = selectedOption.value;
-    const currentQuestionData = getCurrentQuestion();
-    const correctAnswer = currentQuestionData.correctAnswer;
-  
-    if (selectedValue === correctAnswer) {
-      selectedOption.classList.add('correct');
-      resultA2.style.background = "white";
-      resultA2.textContent = 'Correct!';
-      incrementScore();
-    } else {
-      selectedOption.classList.add('incorrect');
-      resultA2.style.background = "white";
-      resultA2.textContent = `Incorrect. The correct answer is: ${currentQuestionData.Answer[correctAnswer]}`;
-    }
-  
-    incrementCurrentQuestion();
-  
-    if (currentQuestion < getTotalQuestions()) {
-      setTimeout(loadQuestion, 900);
-    } else {
-      submitButton.style.visibility = 'visible';
-      submitButton.addEventListener('click', showResults);
-    }
-  }
-  
-  function showResults() {
-    const totalQuestions = getTotalQuestions();
-    const percentage = getScorePercentage();
-    renderResult(score, totalQuestions, percentage);
-    submitButton.style.visibility = 'hidden';
-  }
-  
-  // Initialize the quiz
-  fetchQuizData().then(loadQuestion);
-  
-  submitButton.style.visibility = 'hidden';
-  submitButton.addEventListener('click', checkAnswer);
+  const currentQuestionData = getCurrentQuestion();
+  renderQuestion(currentQuestionData);
+}
+
+function checkAnswer(event) {
+const selectedOption = event.target;
+const selectedValue = selectedOption.value;
+const currentQuestionData = getCurrentQuestion();
+const correctAnswer = currentQuestionData.correctAnswer;
+
+if (selectedValue === correctAnswer) {
+  selectedOption.classList.add('correct');
+  resultA2.style.background = "white";
+  resultA2.textContent = 'Correct!';
+  incrementScore();
+} else {
+  selectedOption.classList.add('incorrect');
+  resultA2.style.background = "white";
+  resultA2.textContent = `Incorrect. The correct answer is: ${currentQuestionData.Answer[correctAnswer]}`;
+}
+
+incrementCurrentQuestion();
+
+if (currentQuestion < getTotalQuestions()) {
+  setTimeout(loadQuestion, 900);
+} else {
+  submitButton.style.visibility = 'visible';
+  submitButton.addEventListener('click', showResults);
+}
+}
+
+function showResults() {
+const totalQuestions = getTotalQuestions();
+const percentage = getScorePercentage();
+renderResult(score, totalQuestions, percentage);
+submitButton.style.visibility = 'hidden';
+}
+
+// Initialize the quiz
+fetchQuizData().then(loadQuestion);
+
+submitButton.style.visibility = 'hidden';
+submitButton.addEventListener('click', checkAnswer);
 
